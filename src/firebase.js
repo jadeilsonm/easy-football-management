ee82ff30e980910cc96b89917f28a776b91425b8
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-11-10 15:02:13 -0300

    chore: add values in variables of ambient

--- src/firebase.js
+++ src/firebase.js
@@ -2,29 +2,24 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
-const mode = import.meta.env.MODE;
-console.log('mode:', mode);
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyB6DT1ueMDOPNFJAt3aHKvX_XAnjM2Qdag",
-  authDomain: "easy-football-management-7ecf6.firebaseapp.com",
-  projectId: "easy-football-management-7ecf6",
-  storageBucket: "easy-football-management-7ecf6.firebasestorage.app",
-  messagingSenderId: "343876823632",
-  appId: "1:343876823632:web:0dfbfbee50138b6c733f55",
-  measurementId: "G-2F6QVC69S0"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
-
 
 const auth = getAuth(firebaseApp);
 
 export const db = getFirestore(firebaseApp);
 
-if ( mode === 'development') {
+if ( import.meta.env.MODE === 'development') {
   connectFirestoreEmulator(db, 'localhost', 8080);
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
-
 }
 
