0732a7168d77bb87e149dd36532a77921739404e
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-12-02 18:49:40 -0300

    feat: modal and edit profile

---
+++ src/components/LoadComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div v-if="isLoading" class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
