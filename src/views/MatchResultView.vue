52a22da83f2d76987343fea269566f60e9548bc0
Jadeilon Silva <jadeilsonm17@gmail.com>
2025-01-25 22:51:37 -0300

    ref: refactor MatchResult

--- src/views/MatchResultView.vue
+++ src/views/MatchResultView.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { onMounted, reactive } from "vue";
+import { onMounted, ref } from "vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useRoute } from 'vue-router'
 import router from "@/router";
 import { DAOChanpionShip, DAOPlayers } from "@/services";
 const auth = getAuth();
 const route = useRoute();
-const state = reactive({
-  ChanpionShip: null,
-  team1: {},
-  team2: {}
-})
+
+const chanpionShip = ref(null);
+const teamOne = ref({players: []});
+const teamTwo = ref({players: []});
+
 
 const { championshipID, numberRound, numberMatch } = route.params;
 
@@ -19,36 +19,57 @@ const backUrl = () => {
 };
 
 const saveMacthResult = async () => {
-  state.ChanpionShip.matches[numberRound].games[numberMatch].team1 = state.team1
-  state.ChanpionShip.matches[numberRound].games[numberMatch].team2 = state.team2
-
-  await DAOChanpionShip.update(championshipID, state.ChanpionShip)
-};
-
-const resquestDefault = async () => {
-
-  const responseChanpionShip = await DAOChanpionShip.getById(championshipID);
-  const x = responseChanpionShip.matches
-    .filter(round => round.round == numberRound);
-  state.ChanpionShip = responseChanpionShip;
-  state.team1 = x[0].games[numberMatch].team1
-  state.team1.players = []
-  state.team2 = x[0].games[numberMatch].team2
-  state.team2.players = []
-  if (!responseChanpionShip.matches[numberRound].games[numberMatch].team1.players) {
-    console.log('123')
-    const playersTeam1 = await DAOPlayers.getByField('teamId', state.team1.teamId);
-    playersTeam1.forEach(player => {
-      state.team1.players = [...state.team1.players, { ...player, gols: 0, redCard: 0, yellowCard: 0 }]
-    });
-    const playersTeam2 = await DAOPlayers.getByField('teamId', state.team2.teamId);
-
-    playersTeam2.forEach(player => {
-      state.team2.players = [...state.team2.players, { ...player, gols: 0, redCard: 0, yellowCard: 0 }]
+  let state = chanpionShip.value;
+  if (state.type == 'cup') {
+    state.matches.forEach(match => {
+      if (match.round == numberRound) {
+        match.games[numberMatch].teamOne = teamOne.value;
+        match.games[numberMatch].teamTwo = teamTwo.value;
+      }
     });
   } else {
-    state.team1 = responseChanpionShip.matches[numberRound].games[numberMatch].team1
-    state.team2 = responseChanpionShip.matches[numberRound].games[numberMatch].team2
+    state.matches[numberRound].games[numberMatch].teamOne = teamOne.value
+    state.matches[numberRound].games[numberMatch].teamTwo = teamTwo.value
+  }
+  await DAOChanpionShip.update(championshipID, state)
+};
+
+const isExistePlayesInTeam = (matchs) => {
+  if (matchs.filter(round => round.round == numberRound)[0].games[numberMatch].team1.players){
+    return matchs
+    .filter(round => round.round == numberRound)[0].games[numberMatch].team1.players.length > 0 && matchs
+    .filter(round => round.round == numberRound)[0].games[numberMatch].team2.players.length > 0
+  } return false;
+}
+
+const returnTeam = (responseChanpionShip, team) => {
+  return responseChanpionShip.matches.filter(round => round.round == numberRound)[0].games[numberMatch][team]
+}
+
+const resquestDefault = async () => {
+  const responseChanpionShip = await DAOChanpionShip.getById(championshipID);
+  chanpionShip.value = responseChanpionShip;
+  const isPlayesExist = isExistePlayesInTeam(responseChanpionShip.matches)
+  const roundMatch = responseChanpionShip.matches
+    .filter(round => round.round == numberRound);
+  if (!isPlayesExist) {
+    let teamOneWithPlayes = roundMatch[0].games[numberMatch].team1;
+    teamOneWithPlayes.players = [];
+    const playersteamOne = await DAOPlayers.getByField('teamId', teamOneWithPlayes.teamId);
+    playersteamOne.forEach(player => {
+      teamOneWithPlayes.players = [...teamOneWithPlayes.players, { ...player, gols: 0, redCard: 0, yellowCard: 0 }]
+    });
+    teamOne.value = teamOneWithPlayes;
+    let teamTwoWithPlayes = roundMatch[0].games[numberMatch].team2;
+    teamTwoWithPlayes.players = [];
+    const playersteamTwo = await DAOPlayers.getByField('teamId', teamTwoWithPlayes.teamId);
+    playersteamTwo.forEach(player => {
+      teamTwoWithPlayes.players = [...teamTwoWithPlayes.players, { ...player, gols: 0, redCard: 0, yellowCard: 0 }]
+    });
+    teamTwo.value = teamTwoWithPlayes;
+  } else {
+    teamOne.value  = returnTeam(responseChanpionShip, 'team1')
+    teamTwo.value  = returnTeam(responseChanpionShip, 'team2')
   }
 }
 
@@ -84,40 +105,39 @@ onMounted(async () => {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(player, index) in state.team1.players" :key="index">
+        <tr v-for="(player, index) in teamOne.players" :key="index">
           <td>
             <span>{{ player.name }}</span>
           </td>
           <td>
             <button type="button" :disabled="player.gols <= 0"
-            @click="() => { state.team1.players[index].gols = player.gols - 1 }">-</button>
+              @click="() => { teamOne.players[index].gols = player.gols - 1 }">-</button>
             <span>{{ player.gols }}</span>
-            <button type="button" @click="() => { state.team1.players[index].gols = player.gols + 1 }">+</button>
+            <button type="button" @click="() => { teamOne.players[index].gols = player.gols + 1 }">+</button>
           </td>
           <td>
             <button type="button" :disabled="player.redCard <= 0 || player.yellowCard == 2"
-            @click="() => { state.team1.players[index].redCard = player.redCard - 1 }">-</button>
+              @click="() => { teamOne.players[index].redCard = player.redCard - 1 }">-</button>
             <span>{{ player.redCard }}</span>
             <button type="button" :disabled="player.redCard >= 1"
-            @click="() => { state.team1.players[index].redCard = player.redCard + 1 }">+</button>
+              @click="() => { teamOne.players[index].redCard = player.redCard + 1 }">+</button>
           </td>
           <td>
             <button type="button" :disabled="player.yellowCard <= 0"
-            @click="() => { state.team1.players[index].yellowCard = player.yellowCard - 1 }">-</button>
+              @click="() => { teamOne.players[index].yellowCard = player.yellowCard - 1 }">-</button>
             <span>{{ player.yellowCard }}</span>
-            <button type="button" :disabled="player.yellowCard >= 2"
-              @click="() => { 
-                state.team1.players[index].yellowCard = player.yellowCard + 1;
-                if (player.yellowCard >= 2) {
-                  state.team1.players[index].redCard = 1;
-                }
+            <button type="button" :disabled="player.yellowCard >= 2" @click="() => {
+              value.teamOne.players[index].yellowCard = player.yellowCard + 1;
+              if (player.yellowCard >= 2) {
+                value.teamOne.players[index].redCard = 1;
+              }
             }">+</button>
           </td>
         </tr>
-        
+
       </tbody>
     </table>
-    
+
     <table>
       <thead>
         <tr>
@@ -128,41 +148,41 @@ onMounted(async () => {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(player, index) in state.team2.players" :key="index">
+        <tr v-for="(player, index) in teamTwo.players" :key="index">
           <td>
             <span>{{ player.name }}</span>
           </td>
           <td>
             <button type="button" :disabled="player.gols <= 0"
-            @click="() => { state.team2.players[index].gols = player.gols - 1 }">-</button>
+              @click="() => { teamTwo.players[index].gols = player.gols - 1 }">-</button>
             <span>{{ player.gols }}</span>
-            <button type="button" @click="() => { 
-              state.team2.players[index].gols = player.gols + 1 
+            <button type="button" @click="() => {
+              teamTwo.players[index].gols = player.gols + 1
             }">+</button>
           </td>
           <td>
             <button type="button" :disabled="player.redCard <= 0 || player.yellowCard == 2"
-            @click="() => { state.team2.players[index].redCard = player.redCard - 1 }">-</button>
+              @click="() => { teamTwo.players[index].redCard = player.redCard - 1 }">-</button>
             <span>{{ player.redCard }}</span>
             <button type="button" :disabled="player.redCard >= 1"
-            @click="() => { state.team2.players[index].redCard = player.redCard + 1 }">+</button>
+              @click="() => { teamTwo.players[index].redCard = player.redCard + 1 }">+</button>
           </td>
           <td>
             <button type="button" :disabled="player.yellowCard <= 0"
-            @click="() => { state.team2.players[index].yellowCard = player.yellowCard - 1 }">-</button>
+              @click="() => { teamTwo.players[index].yellowCard = player.yellowCard - 1 }">-</button>
             <span>{{ player.yellowCard }}</span>
-            <button type="button" :disabled="player.yellowCard >= 2"
-              @click="() => { state.team2.players[index].yellowCard = player.yellowCard + 1;
+            <button type="button" :disabled="player.yellowCard >= 2" @click="() => {
+              teamTwo.players[index].yellowCard = player.yellowCard + 1;
               if (player.yellowCard >= 2) {
-                  state.team1.players[index].redCard = 1;
+                teamTwo.players[index].redCard = 1;
               }
             }">+</button>
           </td>
         </tr>
-        
+
       </tbody>
     </table>
-    
+
   </div>
   <button type="button" @click="saveMacthResult()">Salvar</button>
 </template>
