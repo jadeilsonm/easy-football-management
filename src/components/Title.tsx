5595990dbd749219dfa4fff9a911089ae2c243bd
ElielSilva <silvaeliel940@gmail.com>
2025-06-21 00:33:51 -0300

    created routes initial

---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+
+export const Title = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Torneio TEste
+    </h1>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,46 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import { Title } from './components/Title.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      // { path: "about", Component: About },
+      // {
+      //   path: "auth",
+      //   Component: AuthLayout,
+      //   children: [
+      //     { path: "login", Component: Login },
+      //     { path: "register", Component: Register },
+      //   ],
+      // },
+      {
+        path: "home/a",
+        Component: Title,
+        children: [
+          { path: "trending", Component: Title },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/title",
+    Component: Title,
+    children: [
+      { path: "trending", Component: Title },
+    ],
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
