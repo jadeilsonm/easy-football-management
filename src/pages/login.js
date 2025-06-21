9522f68dafbdd32108f1aa855a29b17d080e0524
jadeilsonm <jadeilsonm17@gmail.com>
2025-06-20 23:18:23 -0300

    init

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+export const Login = () => {
+  return (
+    <div class="@container">
+      <div class="@lg:text-sky-400">
+        <h1 class="@text-3xl @font-bold">Login</h1>
+        <p class="@text-gray" />
+      </div>
+    </div>
+  )
+}
+
+
+
